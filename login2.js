92dbd8e8236d41013c1a2bed0c0fd70955478ecd
hetsprajapati <167339281+hetsprajapati@users.noreply.github.com>
2024-04-17 22:25:26 +0530

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,49 @@
+function validate()
+{
+    var username=document.getElementById("username").value;
+    var password=document.getElementById("password").value;
+    if(username=="admin" && password=="hetsp")
+    {
+        alert("login successfully");
+        window.open("contact.html");
+        return false;
+    
+    }
+
+    else{
+        alert("login failed");
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
